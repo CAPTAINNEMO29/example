78a90c242333b09ae6fd0c56d8870e73770026e8
captainnemo29 <sifu.blanco@gmail.com>
2019-12-15 15:42:28 +0100

    full

---
+++ example-master/src/components/MiddleContainer.vue
@@ -0,0 +1,154 @@
+<template>
+  <section class="middle-container">
+    <div class="profile">
+      <img
+        class="picture-profile"
+        src="images/matrix-3109378_640.jpg"
+        alt="personal picture"
+      />
+      <div class="profile__info">
+        <h2>
+          Tecnologia basada en los principios de consenso, descentralizacion y
+          prueba de trabajo.
+        </h2>
+        <p></p>
+      </div>
+    </div>
+    <hr />
+    <div class="skills">
+      <h2></h2>
+      <div class="skill-row">
+        <img class="" src="" alt="" />
+        <h3>Noticias actuales</h3>
+        <router-link class="atribute" to="/Articulos">Noticias</router-link>
+        <p></p>
+      </div>
+      <div class="skill-row">
+        <img class="" src="" alt="" />
+        <h3>Desarollo de Blocckhain</h3>
+        <p>Documentos y principios basicos.</p>
+      </div>
+    </div>
+    <hr />
+    <div class="contact-me">
+      <h2>Primeros Articulos.</h2>
+      <router-link class="atribute" to="/Bitcoin">Bitcoin</router-link>
+      <router-link class="atribute" to="/Ethereum">Ethereum</router-link>
+      <h3>seguir en contacto</h3>
+      <p>Aprender juntos.</p>
+      <a class="btn" href="neofrom29@gmail.com">CONTACT ME</a>
+      <hr />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MiddleContainer"
+};
+</script>
+
+<style scoped>
+.middle-container {
+  background-color: #b7fbff;
+  width: 100%;
+  height: 40%;
+  display: inline-block;
+}
+
+.profile {
+  background-color: aqua;
+  font-family: "Times New Roman", Times, serif;
+  display: flex;
+  padding: 50px 20px;
+  align-items: center;
+}
+
+.profile__info {
+  flex: 1;
+}
+
+.skills {
+  display: flex;
+  padding: 15px 15px;
+}
+
+.skill-row {
+  flex: 1;
+}
+
+.btn {
+  background: #1090e6;
+  background-image: -webkit-linear-gradient(top, #1090e6, #0d6da8);
+  background-image: -moz-linear-gradient(top, #1090e6, #0d6da8);
+  background-image: -ms-linear-gradient(top, #1090e6, #0d6da8);
+  background-image: -o-linear-gradient(top, #1090e6, #0d6da8);
+  background-image: linear-gradient(to bottom, #1090e6, #0d6da8);
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 18px;
+  padding: 10px 20px 10px 20px;
+  text-decoration: none;
+}
+
+.butn:hover {
+  background: #3cb0fd;
+  background-image: -webkit-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -moz-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -ms-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: -o-linear-gradient(top, #3cb0fd, #3498db);
+  background-image: linear-gradient(to bottom, #3cb0fd, #3498db);
+  text-decoration: none;
+}
+
+.picture-profile {
+  width: 30%;
+  height: 20%;
+  border-radius: 100%;
+}
+
+.atribute {
+  text-align: center;
+  font-size: 22px;
+}
+
+@media (max-width: 640px) {
+  .profile_info {
+    text-align: bottom;
+    flex: 0;
+  }
+
+  .picture-profile {
+    position: static;
+    width: 40%;
+    height: 30%;
+    border-radius: 100%;
+  }
+}
+
+/*@media (max-width: 640px) {
+  .profile {
+    background-color: aqua;
+    font-family: "Times New Roman", Times, serif;
+    position: relative;
+    padding: 50px 20px;
+    align-items: center;
+  }
+
+  .profile_info {
+    text-align: bottom;
+    position: absolute;
+    padding: 5%;
+  }
+
+  .picture-profile {
+    position: static;
+    width: 40%;
+    height: 30%;
+    border-radius: 100%;
+  }
+}*/
+</style>
---
+++ example-master/src/pages/Ethereum.vue
@@ -0,0 +1,90 @@
+<template>
+  <section>
+    <img class="Ethereum" src="images/ethereum.jpg" alt="Ethereum" />
+
+    <h1>Vitalik Buterin and POS.</h1>
+    <p></p>
+
+    <div class="">
+      <img class="" src="" alt="" />
+      <div class="">
+        <h2>The white papper and the yellow papper.</h2>
+        <p>
+          Las tres principales caracteristicas de las que hablaba Vitalik que
+          podrian beneficiar el tener un sistema de turing completo eran:
+        </p>
+
+        <ul>
+          <li>
+            1-la sub-moneda podria ser completanmente almacenada como un
+            contrato.
+          </li>
+          <li>
+            2-El arbol de Merkle de la sub-moneda puede ser almacenado en un
+            contrato, y en la parte superior de la cadena con lo que con cada
+            actualizacion del arbol es validado por el contrato.
+          </li>
+          <li>
+            3-Dos cadenas de bloques pero donde ethereum posee un cliente SPV en
+            la otra cadena.
+          </li>
+        </ul>
+        <a class="Link1" href="https://ethereum.org/">Ethereum.org</a>
+      </div>
+    </div>
+    <hr />
+    <div class="">
+      <h2>What is POS?</h2>
+      <div class="">
+        <img class="" src="" alt="" />
+        <h3></h3>
+        <p>
+          El concepto de Proof of Stake, empezo tambien a ser implementado con
+          la crypto Ethereum. La utilidad en si de esta tecnologia es que no hay
+          una competicion por ser el que tiene mas potencia de computo para
+          generar un bloque, sino que en funcion de las monedas que tengas seras
+          recompensado proporcionalmente. Para que pudiese generarse un bloque
+          malicioso haria falta al menos el 51% de dichas monedas.
+        </p>
+      </div>
+    </div>
+    <hr />
+    <div class="">
+      <h2>EVM</h2>
+      <p>
+        The Ethereum Virtual Machine. Los Smart Contracts estan escritos en
+        Solidity, lenguaje similar a JavaScript y C++. Estos no pueden ser
+        ejecutados directamente por la maquina virtual, primero neecsitan ser
+        compilados a un nivel mas bajo llamado Opcodes.Gracias a estas
+        instrucciones basicas, la maquina virtual puede ejecutar un sistema
+        turing completo.
+      </p>
+      <a class="" href=""></a>
+      <hr />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Ethereum"
+};
+</script>
+
+<style>
+p {
+  padding: 10px, 5px;
+}
+
+.Link1 {
+  font-size: 22px;
+}
+
+.Link2 {
+  font-size: 22px;
+}
+
+li {
+  text-align: justify;
+}
+</style>
